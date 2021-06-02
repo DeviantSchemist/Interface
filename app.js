38929eb73f45fff17a98feee3346e3e6e0f65b29
Deviant <michaelngo1337@yahoo.com>
2021-06-02 12:57:57 -0700

    got inquirer to display on terminal

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const inquirer = require('inquirer')
 const chalk = require('chalk')
+
+inquirer
+  .prompt([
+    {
+      name: 'Name',
+      message: 'What is your name?',
+      type: 'input'
+    }
+  ])
+  .then (answers => {
+    console.log(`Answer is: ${chalk.blue(answers.Name)}`)
+  })
+  .catch(err => console.error(err))
